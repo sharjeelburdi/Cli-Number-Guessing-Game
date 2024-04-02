2a841b5357f81affe24c023a9df5ddc92e2adffd
Sharjeel Ahmad <sharjeelbuledi1@gmail.com>
2024-04-03 01:17:12 +0500

    A simple CLI-Number-Guessing-Game

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Cli-Number-Guessing-Game" 
---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+console.log("\tWelcome to my Number Guessing Game");
+
+const number = Math.floor(Math.random() * 5 + 1);
+
+let input = await inquirer.prompt([
+  {
+    type: "number",
+    name: "userInput",
+    message: "Enter Any Number Between 1 to 5 => ",
+  },
+]);
+if (input.userInput === number) {
+  console.log("Congrats! You Guessed it right.");
+} else {
+  console.log(`Oops! You guessed it wrong. The Number was ${number}.`);
+}
